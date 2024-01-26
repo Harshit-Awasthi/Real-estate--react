d555dfa169b07e9a8e39579d7efab5af5c0c47f1
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-01-26 18:26:17 +0530

    mvc part-1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import {userRoute} from './routes/userRoute.js';
 dotenv.config()
 
 const app = express();
@@ -17,5 +18,7 @@ app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
 
 });
+
+app.use('/api/user',userRoute)
 
 
