80cc28bc66a6de6bcb314defbe6cfb49d60fd03b
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-01-30 01:16:44 +0530

    mvc part 2

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -8,7 +8,22 @@ export const createUser = asyncHandler(async(req,res) => {
 
     let {email} = req.body;
 
-    console.log(email);
-    
-    
-})
+    const userExists = await prisma.user.findUnique({where:{email: email}})
+
+    if(!userExists) {
+
+        const user= await prisma.user.create({data:req.body});
+
+        res.send({
+
+            message: "User created successfully",
+            user: user,
+
+        });
+
+    }
+
+    else res.status(201).send({message: "User already registered"});
+
+
+});
--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import {userRoute} from './routes/userRoute.js';
+import { residencyRoute } from "./routes/residencyRoute.js";
+
+
 dotenv.config()
 
 const app = express();
@@ -20,5 +23,5 @@ app.listen(PORT, ()=>{
 });
 
 app.use('/api/user',userRoute)
-
+app.use("/api/residency",residencyRoute)
 
